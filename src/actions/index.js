af18f7423c1602e8ba91c433217822055c289452
stevekutz <stkutz@gmail.com>
2019-04-30 21:09:01 -0500

    built up reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,9 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from "axios";
+export const FETCH_CHARS = "FETCH_CHARS";
+export const FETCH_GOOD = "FETCH_GOOD";
+export const FETCH_FAIL = "FETCH_FAIL";
+

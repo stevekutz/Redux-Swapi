91760a1f85e1b983aa5a4cc4b97f704d316d1d24
stevekutz <stkutz@gmail.com>
2019-04-30 22:30:53 -0500

    some styling added

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -5,6 +5,21 @@ import { CharacterList } from "../components";
 // import actions
 
 import {getChars} from "../actions";
+import Loader from "react-loader-spinner"; // gives us a spinner !!!
+
+
+import yoda from '../img/yoda_1.png';
+import chewie from '../img/chewie.jpg';
+const simpleDiv = {
+  display: "flex",
+  width: "50%",
+  justifyContent: "center",
+};
+
+const starwarsIMG = {
+  maxHeight: "150px",
+  margin: "2px",
+};
 
 class CharacterListView extends React.Component {
  /* constructor() {
@@ -19,10 +34,32 @@ class CharacterListView extends React.Component {
   render() {
     if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
+      console.log('fetching NOW');
     }
     return (
-      <div className="CharactersList_wrapper">
+      <div className="charactersList_wrapper">
+        {this.props.fetchingChars && (
+          <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />
+        )}
+
+        <div style = {simpleDiv}>
+          <img
+            src = {yoda}
+            alt = "yoda"
+            style = {starwarsIMG}
+          />
+        </div>
+
         <CharacterList characters={this.props.characters} />
+
+        <div style = {simpleDiv}>
+          <img
+            src = {chewie}
+            alt = "chewie"
+            style = {starwarsIMG}
+          />
+        </div>
+
       </div>
     );
   }
